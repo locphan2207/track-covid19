1d9865e7b27932b56f2db3985341c61e97649ac5
Tan Loc Phan <locphan2207@gmail.com>
2020-04-13 03:53:35 -0700

    Add menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,16 @@ import React from "react"
 import "./Menu.css"
 
 function Menu() {
-  return <div className="menu"></div>
+  return (
+    <>
+      <div className="fill-menu"></div>
+      <div className="menu">
+        <div className="menu-home">
+          <h2>Home</h2>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Menu
