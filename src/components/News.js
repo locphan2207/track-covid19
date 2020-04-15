f763b849841c99e80a35cd305ee373e0ba237ef0
Tan Loc Phan <locphan2207@gmail.com>
2020-04-14 22:10:31 -0700

    Finish WHO news

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,42 @@
 import React from "react"
+import { connect } from "react-redux"
 
 import "./News.css"
 
-function News() {
-  return <div className="news"></div>
+function News({ who }) {
+  return (
+    <div className="news">
+      {!!who && (
+        <>
+          <p>{who.title}</p>
+          <div className="carousel-container">
+            <div className="carousel">
+              {who.items.map((item) => {
+                const tokens = item.description.split(" ")
+                const summary = `${tokens.slice(0, 40).join(" ")}${
+                  tokens.length > 40 ? " ..." : ""
+                }`
+                return (
+                  <div className="carousel-item" key={item.guid}>
+                    <a href={item.link} target="_blank">
+                      <i className="flaticon-out" />
+                    </a>
+                    <p className="item-title">{item.title}</p>
+                    <p className="item-date">{item.pubDate}</p>
+                    <p className="item-summary">{summary}</p>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
 
-export default News
+const mapStateToProps = (state) => {
+  return { who: state.news.who }
+}
+
+export default connect(mapStateToProps)(News)
