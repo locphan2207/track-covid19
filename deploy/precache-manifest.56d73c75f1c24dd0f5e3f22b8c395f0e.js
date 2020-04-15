6429d6e0f86556203b7a6079419a878c3fd346c7
Tan Loc Phan <locphan2207@gmail.com>
2020-04-15 03:48:33 -0700

    Deploy files

---
+++ deploy/precache-manifest.56d73c75f1c24dd0f5e3f22b8c395f0e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d04131c167ab14d31b33632e1b1cdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd227d4ec1844926287d",
+    "url": "/static/css/main.85886598.chunk.css"
+  },
+  {
+    "revision": "459a586043ff36c9ecf6",
+    "url": "/static/js/2.2e2f8897.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.2e2f8897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd227d4ec1844926287d",
+    "url": "/static/js/main.2324cc92.chunk.js"
+  },
+  {
+    "revision": "475e629f97b572cbd433",
+    "url": "/static/js/runtime-main.82a2a924.js"
+  },
+  {
+    "revision": "0ca7b09d6560f01c2bedb2200616c77c",
+    "url": "/static/media/Flaticon.0ca7b09d.woff2"
+  },
+  {
+    "revision": "52cf15c5af6219be6073c99a9cf48bd7",
+    "url": "/static/media/Flaticon.52cf15c5.ttf"
+  },
+  {
+    "revision": "759c8982775bdd819b88c0cb3830bfba",
+    "url": "/static/media/Flaticon.759c8982.woff"
+  },
+  {
+    "revision": "ea578afa950fde1781c782ea6767eed8",
+    "url": "/static/media/Flaticon.ea578afa.eot"
+  },
+  {
+    "revision": "f09cb7c4a3ce08673c39b300d4f987fa",
+    "url": "/static/media/Flaticon.f09cb7c4.svg"
+  }
+]);
